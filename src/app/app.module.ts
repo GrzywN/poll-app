ff181a647794a3db0ab3064d3eb147b2c66f2909
Karol Binkowski <starystachu9@gmail.com>
2023-04-14 19:34:10 +0200

    feat: add styled base PollCreator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PollCreatorComponent } from './poll-creator/poll-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollCreatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/poll-creator/poll-creator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poll-creator',
+  templateUrl: './poll-creator.component.html',
+  styleUrls: ['./poll-creator.component.scss']
+})
+export class PollCreatorComponent {
+
+}
